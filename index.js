571cf7d0774dc419df4b64b9c597cd6093e3410e
Leonardo Lopes <leuuu1010@gmail.com>
2023-06-20 18:58:56 -0300

    Arrumando bugs

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const logger = require('morgan');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
-const swaggerUi = require('swaggeer-ui-express');
+const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger/swagger_output.json');
-const swaggerOptions = {customCssUrl: '/swagger-ui.css'};
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
 
+const usersRouter = require('./routes/users');
 
-const  usersRouter = require('./routes/users');
-
-const  app = express();
+const app = express();
 require('dotenv').config();
-
 
 app.use(cors());
 app.use(logger('dev'));
@@ -21,11 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res) => {/* #swagger.ignore = true */ res.redirect('/doc');});
-app.use('/doc',swaggerUi.serve, swaggerUi.setup(swaggerFile,swaggerOptions));
+app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 app.use('/users', usersRouter);
 
-if(process.env.NODE_ENV !== 'test'){
+
+if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
+
+module.exports = app;
--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -1,6 +1,4 @@
 const mongooseToSwagger = require('mongoose-to-swagger');
-const EsquemaTarefa = require('../src/models/tarefa.js');
-const EsquemaUsuario = require('../src/models/usuario.js');
 const swaggerAutogen = require('swagger-autogen')({
     openapi: '3.0.0',
     language: 'pt-BR',
@@ -10,28 +8,29 @@ const outputFile = './swagger_output.json';
 const endpointsFiles = ['../index.js'];
 
 let doc = {
-    info:{
-        version:"1.0.0",
-        tilte: "API do BoardTasks",
+    info: {
+        version: "1.0.0",
+        title: "API do BoardTasks",
         description: "Documentação da API do BoardTasks."
     },
     servers: [
         {
             url: "http://localhost:4000/",
-            description:"Servidor localhost."
+            description: "Servidor localhost."
         },
         {
             url: "https://boardtasks-back.vercel.app/",
-            description : "Servidor de produção."
+            description: "Servidor de produção."
         }
     ],
-    consumes:['application/json'],
-    produces: ['application/json'],
- }
+    consumes: ['application/json'],
+    produces: ['application/json']
+}
 
- swaggerAutogen(outputFile,endpointsFiles,doc).then (()=>{
-    console.log("Documentação do Swagger encontra-se no arquivo em: " + outputFile);
-    if(process.env.NODE_ENV !=='production'){
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em: " + outputFile);
+    if (process.env.NODE_ENV !== 'production') {
         require("../index.js");
     }
- })
+})
